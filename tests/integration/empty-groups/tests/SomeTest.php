104262683c45591055ea5fc0b20bebc7faefd9bf
Cyril Wanner <info@cyr.li>
2020-12-03 21:09:32 +0000

    Add integration tests for warnings

---
+++ tests/integration/empty-groups/tests/SomeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Skills17\PHPUnit\Test\Integration\EmptyGroups;
+
+use Skills17\PHPUnit\BaseTest;
+
+class SomeTest extends BaseTest
+{
+    public function testAFoo()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCFoo()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/integration/ungrouped-tests/tests/SomeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Skills17\PHPUnit\Test\Integration\UngroupedTests;
+
+use Skills17\PHPUnit\BaseTest;
+
+class SomeTest extends BaseTest
+{
+    public function testAFoo()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testBFoo()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testBBar()
+    {
+        $this->assertTrue(true);
+    }
+}
